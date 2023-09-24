900971f8d336e02ebfde949cb515e49fb7ffcf74
ShaunFernandes22 <111562711+ShaunFernandes22@users.noreply.github.com>
2023-09-24 21:28:17 +0530

    created functions

--- POA/booth.py
+++ POA/booth.py
@@ -1,67 +1,92 @@
-# A -> accumulator, M -> Multiplicand, Q->Multiplier, Q1 -> extra_bit, count
-
-# input M & Q in decimal
-M = int(input("Enter the multiplicand in decimal form : "))
-Q = int(input("Enter the multiplier in decimal form : "))
-
-# number of iterations according to binary length (zeroes in accumulator)
-count = max(len(bin(abs(M))[2:]), len(bin(abs(Q))[2:]), 4)
-
+# addition of binary strings
+def add(A, M):
+    carry = 0
+    result = ""
+    for i in range(len(A)-1,-1, -1):
+        res = int(A[i]) + int(M[i]) + carry
+        if res == 2:
+            carry = 1
+            res = 0
+        elif res == 3:
+            carry = 1    
+            res = 1
+        else:
+            carry = 0
+        result = str(res) + result   
+    # ----------Discard carry right here----------
+    # if carry == 1:
+        # result = '1' + result    
+    return result    
+    
 def twosComplement(num):
+    # one's complement 
+    n = len(num)
     comp = ""
-    for i in range(len(num)):
+    for i in range(n):
         if num[i] == '0':
             comp += '1'
         else:
             comp += '0'
-    comp = bin(int(comp, 2) + int('1', 2))[2:]
+    # adding 1        
+    comp = add(comp, '1'.zfill(n))
     return comp
     
-# 4 combinations of M, Q being +ve and -ve    
-if M < 0 and Q < 0:
-    M = twosComplement(bin(abs(M))[2:].zfill(count))
-    Q = twosComplement(bin(abs(Q))[2:].zfill(count))
-elif M < 0 and Q >= 0:
-    M = twosComplement(bin(abs(M))[2:].zfill(count))
-    Q = bin(Q)[2:].zfill(count)
-elif M >=0 and Q < 0:
-    M = bin(M)[2:].zfill(count)
-    Q = twosComplement(bin(abs(Q))[2:].zfill(count))
-else:    
-    M = bin(M)[2:].zfill(count)
-    Q = bin(Q)[2:].zfill(count)
-    
-M_dash = twosComplement(M) # for negative M
-A = "".zfill(count) # accumulator initialized 
-Q1 = '0' # initialize
+# converts to binary strings appropriately    
+def binary(num, count):
+    if num >= 0 :
+        num = bin(num)[2:].zfill(count)
+    else:
+        num = - num
+        num = bin(num)[2:].zfill(count)
+        num = twosComplement(num)
+    return num
 
-def rightShift(A, Q, Q1):
-    if len(A) == count + 1:
-        A = A[1:]
+# all shift to the right by 1 and A_msb stays put
+def rightShift(A, Q, Q1, count):
     Q1 = Q[-1]
     Q = A[-1] + Q[:count - 1]
     A = A[0] + A[:count -1] 
-    
     return A, Q, Q1   
-    
-for _ in range(count):
-    x = Q[-1] + Q1
-    if x == '01':
-        A = bin(int(A,2) + int(M, 2))[2:].zfill(count)
-    elif x == '10':
-        A = bin(int(A,2) + int(M_dash, 2))[2:].zfill(count)
 
-    A, Q, Q1 = rightShift(A, Q, Q1)  
-    print(A, Q, Q1)  
+# ------Booth's algorithm-------
+def boothSigned(M, Q):
+    # iterations is 1 more than 
+    count = max(len(bin(abs(M))[2:]), len(bin(abs(Q))[2:])) + 1
     
-if (A[0] == '1'):
-    print("A + Q = ", A+Q)
-    num = twosComplement(A+Q).zfill(len(A+Q))
-    dec = -int(num, 2)    
-    print("2's complement of A+Q : ")
-else:
-    num = (A+Q)
-    dec = int((A+Q), 2)         
+    # A -> accumulator, M -> Multiplicand, Q->Multiplier, Q1 -> extra_bit, count-> iterations
+    M = binary(M, count)
+    Q = binary(Q, count)
+    M_neg = twosComplement(M) # for negative M
+    A = "".zfill(count) # accumulator initialized 
+    Q1 = '0' # initialized 
+
+    for i in range(count):
+        print(A, Q, Q1)  
+        
+        x = Q[-1] + Q1
+        if x == '01':
+            A = add(A, M)
+        elif x == '10':
+            A = add(A, M_neg)
+        print(A, Q, Q1)  
+
+        A, Q, Q1 = rightShift(A, Q, Q1, count)  
+        print(A, Q, Q1)  
     
-print("The number in binary form is ", num)        
-print("The decimal value of product of signed multiplication is ", dec)
+    if (A[0] == '1'): # negative answer
+        print("A + Q = ", A+Q)
+        bin_num = twosComplement(A+Q)
+        decimal_num = -int(num, 2)    
+        print("2's complement of A+Q : ")
+    else:
+        bin_num = (A+Q)
+        decimal_num = int((A+Q), 2)         
+    return bin_num, decimal_num
+    
+# input M & Q in decimal
+M = int(input("Enter the multiplicand in decimal form : "))
+Q = int(input("Enter the multiplier in decimal form : "))
+
+binary_num, product = boothSigned(M, Q)
+print("The number in binary form is ", binary_num)        
+print("The decimal value of product of signed multiplication is ", product)
